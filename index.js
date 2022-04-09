de295c3c361b2cf3fe3d1281859939db939871e4
Desh Bagley <stemtrainer@gmail.com>
2022-04-09 11:20:09 -0400

    Test 4 failed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const e = document.querySelector("body")
+console.log(e)
+console.log("This has main in it")
+let main = e.querySelector("main")
+ main.remove()
+console.log("main is gone")
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+e.append(newHeader)
+newHeader.innerText = "SCF Coding Academy is the champion"
+//console.log(newHeader.innerText = "SCF Coding Academy is the champion")
+console.log(newHeader.textContent)
+
+
